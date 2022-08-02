a480802bdd3ae4b6fce8465146c1c70efabef323
Matthieu Reungoat <reungoat.matthieu@gmail.com>
2022-08-02 16:25:06 +0200

    post and postman

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-require('./models/dbConfig')
+require('./models/dbConfig');
+const postsRoutes = require('./routes/postsController');
+
+app.use(bodyParser.json());
+app.use('/posts', postsRoutes);
 
 app.listen(5500, () => console.log('Server started: 5500'));
--- routes/postsController.js
+++ routes/postsController.js
@@ -3,8 +3,29 @@ const router = express.Router();
 
 const { PostsModel } = require('../models/postsModel');
 
+// get post
 router.get('/', (req, res) => {
   PostsModel.find((err, docs) => {
+    if (!err) res.send(docs);
+    else console.log('Error to get data: ' + err);
     console.log(docs);
   });
 });
+
+// send post
+router.post('/', (req, res) => {
+  const newRecord = new PostsModel({
+    author: req.body.author,
+    message: req.body.message,
+  });
+
+  newRecord.save((err, docs) => {
+    if (!err) res.send(docs);
+    else console.log('Error creating new data:' + err);
+  });
+});
+
+
+
+module.exports = router;
+ 
